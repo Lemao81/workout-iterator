06d185b13ba8f6246bf17991a96ac0e91a01632e
lemao81 <10658892+Lemao81@users.noreply.github.com>
2025-04-15 07:30:45 +0200

    Add basic state, view components + window initialization

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use iced::Element;
+use iced::widget::{button, center, container};
+
+fn main() -> iced::Result {
+    iced::application("Workout Iterator", App::update, App::view)
+        .window_size((600.0, 400.0))
+        .resizable(false)
+        .run()
+}
+
+#[derive(Default)]
+struct App {}
+
+impl App {
+    fn update(&mut self, _message: Message) {}
+
+    fn view(&self) -> Element<Message> {
+        container(center(button("Next"))).into()
+    }
+}
+
+#[derive(Debug, Clone)]
+enum Message {
+    NextWorkout,
+    AddWorkoutSelected,
 }
