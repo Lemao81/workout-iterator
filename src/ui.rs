57104730f85853addaac77e8d2312446ff5e718e
lemao81 <10658892+Lemao81@users.noreply.github.com>
2025-04-26 07:56:50 +0200

    Unify element return types

--- src/ui.rs
+++ src/ui.rs
@@ -4,7 +4,7 @@ use crate::Message;
 use crate::helper::is_ui_dev;
 use iced::widget::container::Style;
 use iced::widget::{Column, Container, Row, button, center, horizontal_space, text};
-use iced::{Background, Color, Padding};
+use iced::{Background, Color, Element, Padding};
 
 pub const WINDOW_WIDTH: f32 = 500.0;
 pub const WINDOW_HEIGHT: f32 = 300.0;
@@ -18,14 +18,14 @@ pub struct ViewModel {
     pub total: usize,
 }
 
-pub fn create_main_page<'a>(view_model: ViewModel) -> Column<'a, Message> {
+pub fn create_main_page<'a>(view_model: ViewModel) -> impl Into<Element<'a, Message>> {
     Column::new()
         .push(create_header())
         .push(create_body(view_model.workout, view_model.has_next))
         .push(create_footer(view_model.selected_number, view_model.total))
 }
 
-fn create_header<'a>() -> Container<'a, Message> {
+fn create_header<'a>() -> impl Into<Element<'a, Message>> {
     let settings_btn = button("S").on_press(Message::OpenSettings);
 
     let header = center(Row::new().push(horizontal_space()).push(settings_btn));
@@ -41,7 +41,7 @@ fn create_header<'a>() -> Container<'a, Message> {
     container
 }
 
-fn create_body<'a>(workout: String, has_next: bool) -> Container<'a, Message> {
+fn create_body<'a>(workout: String, has_next: bool) -> impl Into<Element<'a, Message>> {
     let text = center(text(workout).size(28));
 
     let button = center(
@@ -63,7 +63,7 @@ fn create_body<'a>(workout: String, has_next: bool) -> Container<'a, Message> {
     container
 }
 
-fn create_footer<'a>(number: i8, total: usize) -> Container<'a, Message> {
+fn create_footer<'a>(number: i8, total: usize) -> impl Into<Element<'a, Message>> {
     let footer = center(
         Row::new()
             .padding(Padding::from(5.0))
--- src/ui/settings_page.rs
+++ src/ui/settings_page.rs
@@ -2,19 +2,18 @@ use crate::Message;
 use iced::Element;
 use iced::widget::{Container, Row, text};
 
-pub fn create_settings_page<'a>() -> Element<'a, Message> {
+pub fn create_settings_page<'a>() -> impl Into<Element<'a, Message>> {
     Container::new(
         Row::new()
             .push(create_workouts_list_view())
             .push(create_button_panel()),
     )
-    .into()
 }
 
 fn create_workouts_list_view<'a>() -> impl Into<Element<'a, Message>> {
     text("list")
 }
 
-fn create_button_panel<'a>() -> impl Into<Element<'a, Message>> + Sized {
+fn create_button_panel<'a>() -> impl Into<Element<'a, Message>> {
     text("buttons")
 }
