f131b02d61551f6f1c7530d27d451ca5ccbe3ad7
lemao81 <10658892+Lemao81@users.noreply.github.com>
2025-04-27 14:56:30 +0200

    Rename state struct

--- src/main.rs
+++ src/main.rs
@@ -10,27 +10,27 @@ use serde::{Deserialize, Serialize};
 
 fn main() -> iced::Result {
     let workouts_state = read_workouts_state();
-    let app = App {
+    let app_state = AppState {
         workouts: workouts_state.workouts,
-        index: workouts_state.index,
+        workout_index: workouts_state.index,
         current_page: Page::Main,
         workout_selection: None,
     };
 
-    iced::application("Workout Iterator", App::update, App::view)
+    iced::application("Workout Iterator", AppState::update, AppState::view)
         .window_size((WINDOW_WIDTH, WINDOW_HEIGHT))
         .resizable(false)
-        .run_with(|| (app, Task::none()))
+        .run_with(|| (app_state, Task::none()))
 }
 
-struct App {
-    index: i8,
+struct AppState {
+    workout_index: i8,
     workouts: Vec<String>,
     current_page: Page,
     workout_selection: Option<String>,
 }
 
-impl App {
+impl AppState {
     fn update(&mut self, message: Message) {
         match message {
             Message::NextWorkout => self.on_next_workout(),
@@ -43,7 +43,7 @@ impl App {
     fn on_next_workout(&mut self) {
         let count = self.workouts.iter().count() as i8;
         if count > 0 {
-            self.index = (self.index + 1) % count;
+            self.workout_index = (self.workout_index + 1) % count;
         }
     }
 
@@ -74,12 +74,16 @@ impl App {
         let workout = self
             .workouts
             .iter()
-            .nth(self.index as usize)
+            .nth(self.workout_index as usize)
             .unwrap_or(&"<empty>".to_owned())
             .clone();
         let total = self.workouts.iter().count();
         let has_next = total > 1;
-        let selected_number = if total == 0 { 0 } else { self.index + 1 };
+        let selected_number = if total == 0 {
+            0
+        } else {
+            self.workout_index + 1
+        };
 
         MainViewModel {
             workout,
