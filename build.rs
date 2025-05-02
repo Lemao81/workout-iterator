92f51a5c80f0c3ff057a4438c6cf3528e9c2eff2
lemao81 <10658892+Lemao81@users.noreply.github.com>
2025-05-02 06:49:03 +0200

    Add build.rs; configure app icon

---
+++ build.rs
@@ -0,0 +1,17 @@
+﻿use winres::WindowsResource;
+
+const ICON_PATH: &str = "./resources/icon.ico";
+
+fn main() {
+    println!("cargo::rerun-if-changed={ICON_PATH}");
+
+    set_icon();
+}
+
+fn set_icon() {
+    if cfg!(target_os = "windows") {
+        let mut res = WindowsResource::new();
+        res.set_icon(ICON_PATH);
+        res.compile().unwrap();
+    }
+}
